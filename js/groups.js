9e8934daf0927787e8715fd9afbaba40315d2260
dave-projects369 <daveknows369@gmail.com>
2026-06-26 11:44:38 +0530

    Create groups.js

---
+++ js/groups.js
@@ -0,0 +1,14 @@
+const groups = {
+  A: { teams: ["Mexico", "South Korea", "South Africa", "Czechia"] },
+  B: { teams: ["Switzerland", "Canada", "Bosnia and Herzegovina", "Qatar"] },
+  C: { teams: ["Brazil", "Morocco", "Haiti", "Scotland"] },
+  D: { teams: ["United States", "Paraguay", "Australia", "Turkey"] },
+  E: { teams: ["Germany", "Curacao", "Ivory Coast", "Ecuador"] },
+  F: { teams: ["Netherlands", "Japan", "Sweden", "Tunisia"] },
+  G: { teams: ["Belgium", "Egypt", "Iran", "New Zealand"] },
+  H: { teams: ["Spain", "Cape Verde", "Saudi Arabia", "Uruguay"] },
+  I: { teams: ["France", "Senegal", "Iraq", "Norway"] },
+  J: { teams: ["Argentina", "Algeria", "Austria", "Jordan"] },
+  K: { teams: ["Portugal", "DR Congo", "Uzbekistan", "Colombia"] },
+  L: { teams: ["England", "Croatia", "Ghana", "Panama"] }
+};
