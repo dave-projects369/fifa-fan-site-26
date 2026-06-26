96849f960603be872e9a7f9074dc56cd284138a3
dave-projects369 <daveknows369@gmail.com>
2026-06-26 12:57:06 +0530

    Create upcomingmatches.js

---
+++ js/upcomingmatches.js
@@ -0,0 +1,16 @@
+const upcomingMatches = [
+  // June 26
+  { team1: "Norway", team2: "France", group: "I" },
+  { team1: "Senegal", team2: "Iraq", group: "I" },
+  { team1: "Egypt", team2: "Iran", group: "G" },
+  { team1: "New Zealand", team2: "Belgium", group: "G" },
+  { team1: "Cape Verde", team2: "Saudi Arabia", group: "H" },
+  { team1: "Uruguay", team2: "Spain", group: "H" },
+  // June 27
+  { team1: "Panama", team2: "England", group: "L" },
+  { team1: "Croatia", team2: "Ghana", group: "L" },
+  { team1: "Algeria", team2: "Austria", group: "J" },
+  { team1: "Jordan", team2: "Argentina", group: "J" },
+  { team1: "Colombia", team2: "Portugal", group: "K" },
+  { team1: "DR Congo", team2: "Uzbekistan", group: "K" },
+];
